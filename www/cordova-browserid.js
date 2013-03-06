412e0f6809f1fc96daa8b8736f212489772f716e
Chris Anderson <jchris@apache.org>
2013-03-06 14:49:01 -0800

    remove the boilerplate www/ directory

--- www/cordova-browserid.js
+++
@@ -1,8 +0,0 @@
-window.presentBrowserIdDialog = function(origin, callback) {
-  // use node.js style error reporting (first argument)
-  cordova.exec(function(assertion){
-    callback(false, assertion);
-  }, function(err) {
-   callback(err);
- }, "CBCordovaBrowserId", "presentBrowserIdDialog", [origin]);
-};
